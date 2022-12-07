ba88a71b590f7c4b422d97d79b9fa9f83e9ee10a
Firdavs_Olimjonov <90975690+FirdavsOlimjonov@users.noreply.github.com>
2022-12-07 14:41:02 +0500

    Update PizzaSplit.java

--- src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
+++ src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -4,8 +4,15 @@ public class PizzaSplit {
     public static void main(String[] args) {
         //Write a program, reading number of people and number of pieces per pizza and then
         //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
+        Scanner scanner = new Scanner(System.in);
+        int numberOfPeople = scanner.nextInt();
+        int numberOfPiece = scanner.nextInt();
 
+        int tmp = Math.max(numberOfPeople,numberOfPiece);
 
+        while (tmp%numberOfPeople!=0 || tmp%numberOfPiece!=0)
+            tmp+=numberOfPiece;
 
+        System.out.println(tmp/numberOfPiece);
     }
 }
